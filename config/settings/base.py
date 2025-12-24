5a584c0bf9e4fd59a4c7e46753ad4235a79f50fb
kit <kitpractice703@gmail.com>
2025-12-24 17:38:16 +0900

    title change

--- config/settings/base.py
+++ config/settings/base.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-from .base import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
