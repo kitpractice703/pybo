63ef167dc882ee4285cdd1bc2a159e2fb4f3d7ba
kit <kitpractice703@gmail.com>
2025-12-26 11:34:22 +0900

    logger

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -2,9 +2,10 @@ from django.core.paginator import Paginator
 from ..models import Question
 from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
+import logging
 
 def index(request):
-  3/0
+  logger.info("INFO 레벨로 출력")
   page = request.GET.get('page', '1') # 페이지
   kw = request.GET.get('kw', '') # 검색어
   question_list = Question.objects.order_by('-create_date')
