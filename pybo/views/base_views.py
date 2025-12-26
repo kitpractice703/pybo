bdc2bf541a35aaa423c9299c844c7fde03fb9e61
kit <kitpractice703@gmail.com>
2025-12-26 11:01:57 +0900

    add

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
 
 def index(request):
+  3/0
   page = request.GET.get('page', '1') # 페이지
   kw = request.GET.get('kw', '') # 검색어
   question_list = Question.objects.order_by('-create_date')
